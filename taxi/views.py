a6f5625209e6b3bee8168c85d2a499f69ee39da5
AKupriichuk <alexa05kup@gmail.com>
2026-01-09 13:17:14 +0200

    c1

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    #template_name = "taxi/manufacturer_list.html"
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car #set
+    context_object_name = "car_list"
+    queryset = Car.objects.select_related("manufacturer").order_by("model")#operation with bd n+1
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    context_object_name = "car"
+
+
+class DriverListView(ListView):
+    model = Driver
+    context_object_name = "driver_list"
+    paginate_by = 5
+    queryset = Driver.objects.order_by("username")
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    context_object_name = "driver"
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
